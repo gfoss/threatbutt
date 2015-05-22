10cf5fb004e9b729a6fe68fdf29d0e0e33179c5a
gfoss <greg.foss@owasp.org>
2015-05-22 14:19:27 -0600

    PowerButt
    Defense in Derpth(tm). Leveraging the glorious ThreatButt API to
    instantly gain attribution on attacks.

---
+++ README.md
@@ -0,0 +1,75 @@
+![ThreatButt](/images/threatbutt.png)
+
+PowerShell Attribution Dice
+
+greg . foss [at] owasp . org
+
+v0.1  --  May 2015
+
+# [About]
+
+ThreatButt + SIEM Integration = Defense in Derpth(TM)
+
+Currently everyone is being attacked, all the time, by people from all over the world. But, how do we know who is actually attacking our networks? Thankfully the folks at ThreatButt have developed a groundbreaking threaty threat attribution feed. Using their patented Clown Strike technology we are able to harness the raw power of private, hybrid, public and cumulus cloudsystem to bring Viking grade threat intelligence to any enterprise.
+
+With this technology we can know instantly who attacked us, from where, using what tech and most importantly, generate loud and progressively more annoying voice-alerts that provide the SOC with 100% accurate information about the attack throughout the day.
+
+# [How To]
+
+This script is super complicated and requires a Computer Science degree to operate:
+
+	PS:\> .\threatbutt.ps1
+
+No need to provide real data, the ThreatButt knows who is attacking you and why...
+
+![Script](/images/script.png)
+
+Make sure you have your volume turned up!
+
+Once the script executes, it creates log files. You can use these to find known malicious activity, just search for the EVID 1337!
+
+![EVIDs](/images/event-logs.png)
+
+If you're a ninja, integrate this with your SIEM so that you know when attacks happen and can announce it throughout the company.
+
+![SmartResponse](/images/SmartResponse.png)
+
+# [Use Cases]
+
+1) Configure as a LogRhythm SmartResponse(TM) to fire for alarms of your choosing!
+
+2) Run this on infected systems to automatically know what APT resides there
+
+3) Execute this remotely on unsuspecting user's systems... Better yet, create a scheduled task!
+
+4) Just run it, yo!
+
+# DISCLAIMER
+
+1) This is an Obvious Parody...
+2) LogRhythm does not actually endorse any of this in any way, shape, or form
+3) All attribution data is randomly generated
+4) We don't even send any real data to their API, for obvious reasons
+5) How do you computer?
+
+# LICENSE
+
+Copyright (c) 2015, Greg Foss
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of Greg Foss nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ threatbutt.ps1
@@ -0,0 +1,87 @@
+#requires -version 3.0
+
+  #======================================#
+  # ThreatButt Virtual Attribution Dice  #
+  # greg . foss [at] owasp . org         #
+  # v0.1  --  May 2015                   #
+  #======================================#
+
+#=======================================================================================
+# ThreatButt Attribution - 100% Accuracy Guaranteed, Every Time
+#=======================================================================================
+
+# Hide errors, because I can't PowerShell
+$ErrorActionPreference = 'silentlycontinue'
+
+# Acurately determine the Attacker's IP address using next-gen attribution techniques developed by the NSA
+function attribution {
+    return [IPAddress]::Parse([String] (Get-Random) ).IPAddressToString
+}
+$buttIP = Write-Output (attribution)
+
+# Talk to the ThreatButt
+$buttHash = Get-Random -input "03bf4710574fcfc04aebeaa802a34d29", "f121eb65604a8e2490318de5dd454e83"
+$buttActor = Invoke-RestMethod -Method POST -Uri "http://threatbutt.io/api" -Body $buttIP
+$buttMalware = Invoke-RestMethod -Method POST -Uri "http://api.threatbutt.io/api/md5/$buttHash"
+if ( $buttActor -eq "Please try again later" ) {
+    $buttActor = "Chris Roberts"
+} if ( $buttActor -eq "Concentrate and ask again" ) {
+    $buttActor = "Squirrel"
+} else {
+}
+
+# Integrate Premium Threat Data Feeds
+$buttLocation = Get-Random -input "office", "kitchen", "dining room", "school", "park", "theater", "datacenter", "basement", "attic", "bathroom", "parking lot", "apple store", "apartment", "house", "airplane", "ISS Space Station"
+$buttDevice = Get-Random -input "mom's laptop", "dad's laptop", "super computer", "speak and spell", "ti 83", "i Pad", "i Phone", "i watch", "tablet", "easy bake oven", "airplane attacker 3000", "Mars Rover"
+$phrase = "Attack detected from $buttActor. In the $buttLocation. With the $buttMalware. Using their $buttDevice..."
+
+# Tell the entire SOC all about the Attack
+[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
+$object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$object.SelectVoiceByHints('Female')
+$object.Speak($phrase)
+
+# Log the Attack and Attribution Data
+New-EventLog -LogName Application -Source "The ThreatButt"
+Write-EventLog -LogName Application -Source "The ThreatButt" -EntryType Information -EventId 1337 -Message "$phrase"
+
+# Tell us who did it!
+echo ""
+$phrase
+<#
+                        ▄▄▄▓▓▓▓▓▓▌▄                                                 
+                     ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓µ                                              
+                   ╓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌                                             
+                  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                                            
+                 ▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▓▓▄▀▓▓▓▓▌                                           
+                ▓▓▓▓▓▓▓▓▓▓▓█▌▓▓▓▓▓Γ▀▓▓▓▓▓                        ,▄▌▌▄▄             
+               ▄▓▓▓▓▓Γ▓▓▓▓▓╓▓▓▓▓▓█  ▓▌▓▓▓▓                      ╫▓.   Γ▀▀██▓▄▄      
+             ╒▓▓▓▓▓ ▀▓╣▓▓▓▓▓▓▓▓▓█ ,▄▓▄▓▓▓▓                     ╓▓^ ╒▄ ,      ▀▓▄    
+             ▓▓▓▓▓█ ╓▓▓▓▓▓▓▄▀█▓▄▓▓▓▓▓▓▓▓▓▓▓                    ▓▌  ▄µ,▀ ▀⌐╓▄  ▓▀    
+             █▓▓▓▄▓█▓▓▓▓▓▓▓▓▓▓▓█▓▓▓██▀╠▓▓▓▓µ                  ▄▓  ▄▄ Γ`╜▀ ▓Γ ╬▓     
+              ╟▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▀▀,▄▓▓▓▓▓██▓                 ╒▓  ╒▄ ƒ▀ █ ▓▌ ╓▓      
+              ▐▓▓▓▓██▓▓▓▓▓█▀Γ,▄▄▓▓▓██▀Γ    ▓▓                ▓▀  ▄µ,▀ ▀⌐▐▓  ▓▀      
+               ▓▄▄▓██▀▀▄▄▄▓▓▓▓██▀.          ▓▓           ▄▄▄▓▓  ╔▄ ▀ Å▀ ▓Γ ▓▓       
+               ▓▓▓▓▓▓▓▓▓▓▀▀Γ        ▄╦ █▌    ▓▓     ▄▓█▀▀ΓΓ▀▓─ ╒▄ ╙▀ⁿ█ ▓▌ ╓▓        
+               └▓▓▀▀Γ                ▓  ▓▄    ▓▓ ╓▓█Γ,    ,▓▓▄▄▄▄▄▓,█⌐▐▓  ▓Γ        
+                █▓                    ▓ ▐▓     ▓▓▓   ▀█▓▓▓▀ΓΓ     Γ▀▀▓▓Γ ▓█         
+                "▓µ                   █▄ █▓     ▓▌  ▀,▓▀              ▀▓▄▓          
+                 ▓▓                    ▓  ▓µ     Γ   ▓Γ     ╕          █▓▀          
+                  ▓▌                   ▐▓ ▐▓               █            ▓           
+                   ▓▓                   █▌ ▓▓▄▄,                       ▓▓           
+                    █▓µ                 ╓▓  ▄▄,▐▓▓        ,▄          ▐▓            
+                     ╙▓▄                ▓Γ▓▄▀▓▀▀▀           ╓▒       ▄▓             
+                       ▓▓╕              ▓▄▓█▓▓                 Φ   ,▓▓              
+                        Γ▓▓              Γ                       ▄▓▓▀               
+                          ╙▓▌,                             ▄▄▄▓▓█▀Γ                 
+                            ▀█▓▄,                     ╓▄▓▌ ╙▓▄                      
+                               Γ▀▓▓▌▄▄,       ,,▄▄▄▓▓█▀Γ└▓  ▓▓                      
+                                   ΓΓ▀▀▀▓▓▀▓▓▀▀▀▀▀Γ ,,,  ▓▓ ╙▓                      
+                                        █▌ ▓▌      ▀▓▓▀▀▀█▓M █▓                     
+                                        ▓▌ ▓▌        ▀▓▓▄,    ▓                     
+                                 ,▄▄▄,  ▓▌ ▓▌           Γ▀██▓▓▓Γ                    
+                                 █▓▄Γ▀█▓▓▌]▓                                        
+                                  ╙█▓▄  Γ ▐▓                                        
+                                     Γ██▓▓▓▀                                        
+        Sweet ASCII Art Stolen from: https://github.com/asshurtmacfags/threatbutt
+#>
